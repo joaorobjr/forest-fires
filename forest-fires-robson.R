1c345a577e60f5af3eb364f5e43f8ee4621cd0b9
joaorobjr <joaorobjr@gmail.com>
2020-12-25 10:55:52 +0000

    dataset com dados do clima

--- forest-fires-robson.R
+++ forest-fires-robson.R
@@ -1,9 +1,12 @@
-library(dplyr)
+library(tidyverse)
 library(funModeling)
-library(stringr)
-library(chron)
 library(measurements)
 library(lubridate)
+library(ggplot2)
+library('rnoaa')
+library(lubridate)
+library(devtools)
+options(noaakey = "mqEuOSuAUjyuGlTjVjxxCpzRlbrooRnr")
 
 #Load raw data --------------------------------------------------
 fires.raw = as_tibble(read.csv("data/fires2015_train.csv",
@@ -90,6 +93,151 @@ df_status(fires.raw)
 save(fires.raw, file = "fires.raw.RData")
 load("fires.raw.RData")
 
+#bar plot of forests fires during 2015
+ggplot(fires.raw, aes(x = month(alert, label = T))) + 
+  geom_bar(fill = "red") + 
+  ggtitle("Distribution of forests fires across 2015") +
+  labs(x="Months", y="Total")
 
 
+# CREATE WEATHER HISTORICAL DATASET ----------------------------------------------
+
+load("data/station_data.Rdata")
+
+get_nearby_stations = function (df, measure){
+  nearby_stations = meteo_nearby_stations(lat_lon_df = df,
+                                          station_data = station_data, 
+                                          radius = 1000, 
+                                          var = measure,
+                                          year_min = 2015, year_max = 2015,
+                                          limit = 10)
+  return(nearby_stations)
+}
+
+get_weather_data = function (station, measure) {
+  wd <- ghcnd_search(station, var = measure, date_min = "2015-01-01", date_max = "2015-12-31")
+  return(wd)
+}
+
+# Create dataframe with all distinct parish, lat and lon. 
+# After Rename columns to names accepted by meteo_nearby_stations method
+df_parish = distinct(fires.raw, parish, lat, lon) %>% 
+  rename(id = parish, latitude = lat, longitude = lon)
+
+weather_measures = c("TAVG", "TMAX", "TMIN", "PRCP")
+
+stationsByParish = get_nearby_stations(df_parish, weather_measures)
+
+weather_data = c()
+
+for (parish in names(stationsByParish)) {
+  station = eval(parse(text=sprintf("stationsByParish$'%s'$id[1]", parish)))
+  wd = get_weather_data(station, weather_measures)
+  weather_data = append(weather_data, eval(parse(text=sprintf("list('%s'= wd)", parish))))
+}
+
+
+#save(weather_data, file = "weather_data.RData")
+load("weather_data.RData")
+
+
+# MERGE WEATHER DATA AND FOREST FIRES -----------------------------------------
+
+# Create new attributes: tavg, tmax, tmin, prcp
+fires.raw = fires.raw %>% mutate(tavg = 0, tmax = 0, tmin = 0, prcp = 0)
+
+for (parish_name in names(weather_data[1:1])) {
+  
+  arr_alert_dates = fires.raw %>% filter(parish == parish_name) %>% select(alert_date)
+  
+  for (dt in arr_alert_dates$alert_date) {
+    
+    wdTAVG = eval(parse(text=sprintf("filter(weather_data$'%s'$tavg, date == '%s')", parish_name, dt)))
+    wdTMAX = eval(parse(text=sprintf("filter(weather_data$'%s'$tmax, date == '%s')", parish_name, dt)))
+    wdTMIN = eval(parse(text=sprintf("filter(weather_data$'%s'$tmin, date == '%s')", parish_name, dt)))
+    wdPRCP = eval(parse(text=sprintf("filter(weather_data$'%s'$prcp, date == '%s')", parish_name, dt)))
+    
+    idx = which(fires.raw$parish == parish_name & fires.raw$alert_date == dt)
+    
+    if(nrows(wdTAVG)>0){
+      fires.raw[idx,]$tavg = wdTAVG$tavg/10
+    }
+    if(nrows(wdTMAX)>0){
+      fires.raw[idx,]$tmax = wdTMAX$tmax/10
+    }
+    if(nrows(wdTMIN)>0){
+      fires.raw[idx,]$tmin = wdTMIN$tmin/10
+    }
+    if(nrows(wdPRCP)>0){
+      fires.raw[idx,]$prcp = wdPRCP$prcp/10
+    }
+  }
+}
+
+
+
+
+parish_name = "A dos Cunhados"
+dt = "2015-08-05"
+wd.tavg = eval(parse(text=sprintf("filter(weather_data$'%s'$tavg, date == '%s')", parish_name, dt)))
+wd.tmax = eval(parse(text=sprintf("filter(weather_data$'%s'$tmax, date == '%s')", parish_name, dt)))
+wd.tmin = eval(parse(text=sprintf("filter(weather_data$'%s'$tmin, date == '%s')", parish_name, dt)))
+wd.prcp = eval(parse(text=sprintf("filter(weather_data$'%s'$prcp, date == '%s')", parish_name, dt)))
+
+dim(wd.tavg)[1] > 0
+dim(wd.tmax)[1] > 0
+
+
+idx = which(fires.raw$parish == parish_name)
+fires.parish = fires.raw[idx,]
+alert_dates = fires.raw[idx,"alert_date"]
+idx2 = which(fires.raw$parish == parish_name && as.Date(fires.raw$alert_date) %in% as.Date("2015-05-17"))
+
+which(fires.raw$parish == parish_name & fires.raw$alert_date == "2015-05-17")
+
+wd.tavg %>% filter(date == "2015-05-17")
+
+
+fires.raw[2403,]
+
+for (dt in alert_dates$alert_date) {
+  wd = wd.tavg %>% filter(date == dt)
+  idx = which(fires.raw$parish == parish_name & fires.raw$alert_date == dt)
+  fires.raw[idx,]$tavg = wd$tavg/10
+}
+
+names(weather_data[1:1])
+
+
+
+freguesia = "Abragão"
+df = eval(parse(text=sprintf("weather_data$'%s'$tavg", freguesia)))
+df_subset = subset(df, as.Date(df$date) %in% as.Date(dates$alert_date[1]))
+
+idx = which(fires.raw$parish == freguesia)
+dates = fires.raw[idx,"alert_date"]
+date = dates[1,]
+
+for (dt in dates[1]) {
+  idx.dt = which(df$date %in% as.Date(dt))
+  df_tavg = df[idx.dt,]
+}
+df_tavg
+
+#fires.raw %>% mutate(tavg = if_else(parish == freguesia && as.Date(alert_date) %in% ) )
+
+
+
+df_subset = subset(df, as.Date(df$date) %in% as.Date(dates$alert_date[1]))
+
+
+
+for (name in names(weather_data)) {
+  df.tavg = eval(parse(text=sprintf("weather_data$'%s'$tavg", name)))
+  
+  
+ 
+  wd = get_weather_data(station, c("TAVG","TMAX","TMIN","PRCP"))
+  weather_data = append(weather_data, eval(parse(text=sprintf("list('%s'= wd)", parish))))
+}
 
